2213158d7611c2376828db5935eaf88a46c9e124
Marodioss <kreker.marouane22@gmail.com>
2022-08-18 01:42:40 +0100

    imprimer buletin avancement

---
+++ GestionEspav/ImprimerBulletin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionEspav
+{
+    public partial class ImprimerBulletin : Form
+    {
+        public ImprimerBulletin()
+        {
+            InitializeComponent();
+        }
+
+        private void ImprimerBulletin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Buletin blt = new Buletin();
+            blt.SetParameterValue("nom",textBox1.Text);
+            blt.SetParameterValue("prenom", textBox2.Text);
+            crystalReportViewer1.ReportSource = blt;
+            crystalReportViewer1.Refresh();
+        }
+    }
+}
--- GestionEspav/MatiereCrud.Designer.cs
+++ GestionEspav/MatiereCrud.Designer.cs
@@ -104,11 +104,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(908, 109);
+            this.button1.Location = new System.Drawing.Point(909, 109);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(177, 58);
             this.button1.TabIndex = 7;
-            this.button1.Text = "Ajouter";
+            this.button1.Text = " ";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
