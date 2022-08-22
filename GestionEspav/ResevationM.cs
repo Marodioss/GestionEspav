119bec524746c0b6f37fb484a823661509fc841e
benbakka <salah2000fettah@gmail.com>
2022-08-22 23:53:27 +0100

    resrvationF

--- GestionEspav/ResevationM.cs
+++ GestionEspav/ResevationM.cs
@@ -21,6 +21,33 @@ namespace GestionEspav
             adapter.Fill(table);
             return table;
         }
+        public bool updateReservation(int id, String nom, String prenom, DateTime dateL, DateTime dateR, int idMateriel, int nbrItem, String retourne)
+        {
+           // MySqlCommand command = new MySqlCommand("UPDATE `enseignant` SET `nom`=@na,`prenom`=@pre,`cin`=@ci,`idmE`=@mat,`telephone`=@tel,`whatsapp`=@wts,`email`=@em,`photo`=@img WHERE  `id`= @id", connect.getconnection);
+            //
+            MySqlCommand command = new MySqlCommand("UPDATE `reservationMat` SET `nom` = @nm, `prenom` = @pr, `dateL` = @dl, `dateR` = @dr, `idMateriel` = @im, `nbrItem` = @nbr, `retourne` = @rt  WHERE `id` = @id", connect.getconnection);
+           
+            command.Parameters.Add("@id", MySqlDbType.Int64).Value = id;
+            command.Parameters.Add("@nm", MySqlDbType.String).Value = nom;
+            command.Parameters.Add("@pr", MySqlDbType.String).Value = prenom;
+            command.Parameters.Add("@dl", MySqlDbType.Date).Value = dateL;
+            command.Parameters.Add("@dr", MySqlDbType.Date).Value = dateR;
+            command.Parameters.Add("@im", MySqlDbType.Int64).Value = idMateriel;
+            command.Parameters.Add("@nbr", MySqlDbType.Int64).Value = nbrItem;
+            command.Parameters.Add("@rt", MySqlDbType.String).Value = retourne;
+
+            connect.openConnect();
+            if (command.ExecuteNonQuery() == 1)
+            {
+                connect.closeConnect();
+                return true;
+            }
+            else
+            {
+                connect.closeConnect();
+                return false;
+            }
+        }
         public string getMaterielNP(string nomMateriel)
         {
             MySqlCommand command = new MySqlCommand("SELECT id FROM `materiels` WHERE nomMateriel = @nm", connect.getconnection);
@@ -45,10 +72,58 @@ namespace GestionEspav
 
             }
         }
-        public bool insertReservationMat(String nom, String prenom, DateTime dateL, DateTime dateR, int idMateriel, int nbrItem)
+        public String requpNbrMatRest(String nomMateriel)
         {
-            MySqlCommand command = new MySqlCommand("INSERT INTO `reservationMateriel`(`nom`, `prenom`, `dateL`, `date`R, `idMateriel`, `nbrItem`)" +
-            "VALUES (@nm,@pr,@dl,@dr,@im,@nbr)", connect.getconnection);
+            MySqlCommand command = new MySqlCommand("select nbrUnite from materiels where nomMateriel = @ide; ", connect.getconnection);
+            command.Parameters.Add("@ide", MySqlDbType.VarChar).Value = nomMateriel;
+
+
+            MySqlDataReader reader;
+            connect.openConnect();
+            reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                String ve = reader["nbrUnite"].ToString();
+                reader.Close();
+                connect.closeConnect();
+                return ve;
+
+            }
+            else
+            {
+                reader.Close();
+                connect.closeConnect();
+                return null;
+            }
+        }
+        public String requpNbrMatRes(int idMateriel)
+        {
+            MySqlCommand command = new MySqlCommand("select sum(nbrItem) from reservationmat where idMateriel = @ide;", connect.getconnection);
+            command.Parameters.Add("@ide", MySqlDbType.Int64).Value = idMateriel;
+
+
+            MySqlDataReader reader;
+            connect.openConnect();
+            reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                String ve = reader["sum(nbrItem)"].ToString();
+                reader.Close();
+                connect.closeConnect();
+                return ve;
+
+            }
+            else
+            {
+                reader.Close();
+                connect.closeConnect();
+                return null;
+            }
+        }
+        public bool insertReservationMat(String nom, String prenom, DateTime dateL, DateTime dateR, int idMateriel, int nbrItem, String retourne)
+        {
+            MySqlCommand command = new MySqlCommand("INSERT INTO `reservationMat`(`nom`, `prenom`, `dateL`, `dateR`, `idMateriel`, `nbrItem`,`retourne`)" +
+            "VALUES (@nm,@pr,@dl,@dr,@im,@nbr,@rt)", connect.getconnection);
           
             command.Parameters.Add("@nm", MySqlDbType.String).Value = nom;
             command.Parameters.Add("@pr", MySqlDbType.String).Value = prenom;
@@ -56,7 +131,9 @@ namespace GestionEspav
             command.Parameters.Add("@dr", MySqlDbType.Date).Value = dateR;
             command.Parameters.Add("@im", MySqlDbType.Int64).Value = idMateriel;
             command.Parameters.Add("@nbr", MySqlDbType.Int64).Value = nbrItem;
-            
+            command.Parameters.Add("@rt", MySqlDbType.String).Value = retourne;
+
+
             connect.openConnect();
             if (command.ExecuteNonQuery() == 1)
             {
--- GestionEspav/reservationMat.cs
+++ GestionEspav/reservationMat.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,7 @@ namespace GestionEspav
         {
             InitializeComponent();
             showTable();
+            LoadcomboClass();
         }
         private void LoadcomboClass()
         {
@@ -44,22 +46,102 @@ namespace GestionEspav
             string prenom = textBox1.Text;
             int idMateriel = int.Parse(resevationM.getMaterielNP(comboBox1.Text));
             int nbrItem = int.Parse(textBox3.Text);
-
-            if (resevationM.insertReservationMat(nom, prenom, dateL, dateR, idMateriel, nbrItem))
+            int a = int.Parse(textBox2.Text);
+            string retourne = radioButton1.Checked ? "Oui" : "Non";
+            if (nbrItem < a)
             {
-                MessageBox.Show("Reservation Ajouter", "L'heure Enseignant", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
+                if (resevationM.insertReservationMat(nom, prenom, dateL, dateR, idMateriel, nbrItem, retourne))
+                {
+                    MessageBox.Show("Reservation Ajouter", " Reservation Ajouter", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    showTable();
+                }
 
-            else
-            {
-                MessageBox.Show("Empty Field", "Reservation ajouter reservation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                else
+                {
+                    MessageBox.Show("Empty Field", "Reservation ajouter reservation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
 
+                }
             }
+            else 
+                MessageBox.Show("Empty Field", "le nombre d'item reserve est supérieure a le nombre d'item dans le stock", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
         }
+
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            textBox5.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox1.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            dateTimePicker1.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            dateTimePicker2.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+           
+            comboBox1.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+            
+            textBox3.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
+            String r = dataGridView1.CurrentRow.Cells[7].Value.ToString();
+            if(r == "Oui")
+            {
+                radioButton1.Checked = true;
+            }
+            else 
+                radioButton2.Checked = true;
 
+
+        }
+
+        private void reservationMat_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int idMateriel = int.Parse(resevationM.getMaterielNP(comboBox1.Text));
+           // int nbrItem = int.Parse(textBox3.Text);
+
+            int nbrMS = int.Parse(resevationM.requpNbrMatRest(comboBox1.Text));
+            int nbrMR = int.Parse(resevationM.requpNbrMatRes(idMateriel));
+            int resNbrMatR = nbrMS - nbrMR;
+            textBox2.Text = resNbrMatR.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DateTime dateL = dateTimePicker1.Value;
+            DateTime dateR = dateTimePicker2.Value;
+            string nom = textBox4.Text;
+            string prenom = textBox1.Text;
+            int idMateriel = int.Parse(comboBox1.Text);
+            int nbrItem = int.Parse(textBox3.Text);
+            int id = int.Parse(textBox5.Text);
+            string retourne = radioButton1.Checked ? "Oui" : "Non";
+
+            {
+                if (resevationM.updateReservation(id, nom, prenom, dateL, dateR, idMateriel, nbrItem, retourne))
+                {
+                    showTable();
+                    MessageBox.Show("Enseignant data update", "Update Enseignant", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    //button3.PerformClick();
+                }
+                //}
+                /*   catch (Exception ex)
+
+                   {
+                       MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                   }
+               }*/
+                else
+                {
+                    MessageBox.Show("Redimentionner la photo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+            }
         }
     }
 }
